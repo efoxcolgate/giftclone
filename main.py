e6552177d82b472c5a03407276148cc8afa0ee76
EC2 Default User <ec2-user@ip-172-31-21-81.us-east-2.compute.internal>
2021-09-10 02:14:06 +0000

    test

--- main.py
+++ main.py
@@ -7,12 +7,40 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Erika!"}
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
+@app.get("/scrabblehelper/{word}")
+async def scrabblehelper(word: str):
+    scores={ 'a':1,
+            'b':3,
+            'c':3,
+            'd':2,
+            'e':1,
+            'f':4,
+            'g':2,
+            'h':4,
+            'i':1,
+            'j':8,
+            'k':5,
+            'l':1,
+            'm':3,
+            'n':1,
+            'o':1,
+            'p':3,
+            'q':10,
+            'r':1,
+            's':1,
+            't':1,
+            'u':1,
+            'v':4,
+            'w':4,
+            'x':8,            
+            'y':4,
+            'z':10}
+    sum1 = 0
+    for char in word:
+        sum1=sum1+scores[char]
 
-    total = num1 + num2
-    return {"total": total}
+
+    return {"score": sum1}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
